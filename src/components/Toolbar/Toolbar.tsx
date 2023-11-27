d59e970fdcb41ce0599b18ed450be5772127dee4
Nurzat Taalaibekova <tnurzat91@gmail.com>
2023-11-27 11:54:24 +0300

    change styles

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -2,17 +2,39 @@ import {NavLink} from "react-router-dom";
 
 const Toolbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <ul className="navbar-nav mr-auto flex-row gap-2 flex-nowrap">
           <li className="nav-item">
-            <NavLink to="/" className="nav-link">Books</NavLink>
+            <NavLink to="/" className="nav-link"
+                     style={({ isActive, isPending, isTransitioning }) => {
+                       return {
+                         fontWeight: isActive ? "bold" : "",
+                         color: isPending ? "green" : "white",
+                         viewTransitionName: isTransitioning ? "slide" : "",
+                       };
+                     }}>Books</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/anime" className="nav-link">Anime</NavLink>
+            <NavLink to="/anime" className="nav-link"
+                     style={({ isActive, isPending, isTransitioning }) => {
+                       return {
+                         fontWeight: isActive ? "bold" : "",
+                         color: isPending ? "green" : "white",
+                         viewTransitionName: isTransitioning ? "slide" : "",
+                       };
+                     }}>Anime</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/movies" className="nav-link">Movies</NavLink>
+            <NavLink to="/movies" className="nav-link"
+                     style={({ isActive, isPending, isTransitioning }) => {
+                       return {
+                         fontWeight: isActive ? "bold" : "",
+                         color: isPending ? "green" : "white",
+                         viewTransitionName: isTransitioning ? "slide" : "",
+                       };
+                     }}
+            >Movies</NavLink>
           </li>
         </ul>
       </div>
